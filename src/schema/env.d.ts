bd1bbfff184ec7a13a73bfbd843c18d4249eaf9f
Zero <mengxiangyu996@qq.com>
2026-02-10 08:48:57 +0800

    chore: init

---
+++ src/schema/env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="vite/client" />
+
+// 声明 vite 环境变量的类型
+interface ImportMetaEnv {
+    readonly VITE_APP_TITLE: string;
+    readonly VITE_APP_VERSION: string;
+    readonly VITE_HOST: string;
+    readonly VITE_PORT: number;
+    readonly VITE_API_BASE_URL: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+    const env: ImportMetaEnv = loadEnv(
+        mode,
+        process.cwd(),
+    ) as unknown as ImportMetaEnv;
+
+    return {
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
+        },
+        server: {
+            host: env.VITE_HOST || "localhost",
+            port: env.VITE_PORT || 8000,
+            open: false,
+            cors: true,
+            strictPort: false,
+            proxy: {
+                "/api/codeup": {
+                    target: "https://openapi-rdc.aliyuncs.com",
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api\/codeup/, ""),
+                },
+            },
+        },
+        build: {
+            target: "modules",
+            outDir: "dist",
+            assetsDir: "assets",
+        },
+    };
+});
